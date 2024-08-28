40ee0b3c86ba9444e258eb9d69f416d8e50ceffc
prihandiva <prihandiva1711@gmail.com>
2024-08-28 17:11:23 +0700

    first commit

---
+++ resources/views/KTP_Fitria.blade.php
@@ -0,0 +1,143 @@
+<html>
+    <head>
+        <title>Membuat KTP</title>
+        <link rel="stylesheet" type="text/css">
+        <style>
+            html,body{
+    margin:0;
+    padding:6px;
+}
+body{
+    font-family:Arial,Helvetica,sans-serif;
+    background:#ddd;
+    padding-bottom:100px;
+}
+.container-satu {
+    max-width : 790px;
+    max-height : 500px auto;
+    margin: 100px;
+    border: 3px solid grey;
+    background-color:#83cefc;
+    box-sizing:border-box;
+    display: flex;
+    flex-direction: column;
+    border-radius:15px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 10);
+
+}
+.details td, .details th {
+    padding: 10px;
+}
+.foto-table{
+    text-align: center;
+    font-family:Arial;
+    margin-right: 20px;
+    margin-bottom : 10 auto;
+    padding-right: 10px; 
+}
+.fontheader{
+    color:black;
+    font-family:Arial;
+    font-style:normal;
+    text-align :center;
+    grid-area: header;
+}
+.NIK{
+    color:black;
+    font-family:OCR A;
+    font-style:bold;
+}
+.details {
+    font-weight: bold;
+    margin-right: 3px;
+    color : black;
+    line-height:1;
+    
+}
+
+.sidebar-satu{
+    width: 1000px;
+    padding:100px ;
+}
+.kolom-utama{
+    width:1000px;
+    order :2 ;
+    padding :10px;
+}
+.sidebar-dua{
+    width : 100px;
+    order :1px ;
+}
+        </style>
+    </head>
+    <body>
+        <div class= "container-satu">
+            <div class="header">
+                <h1 class="fontheader">PROVINSI JAWA TIMUR</h1>
+                <h1 class="fontheader">KOTA MALANG</h1>
+                </div>
+
+                <table class="content" cellpadding="1px">
+                <div class="details">
+                <tr>
+                    <td><h2 class ="NIK">NIK : 3573045711030004</h2></td>
+                    <div class = foto-table>
+                        <td rowspan="10" class="foto-table"> <img src="img/foto.jpg" height="240px" width="180px"></td>
+                    </div>
+                    
+                </tr>
+                
+                <tr>
+                    <td><b>Nama <b>: FITRIA RAMADHANI PRIHANDIVA</b></td>
+                    
+                </tr>
+                <tr>
+                    <td><b>Tempat/Tgl Lahir : MALANG, 17-11-2003</b></td>
+                </tr>
+                <tr>
+                    <td><b>Jenis Kelamin : PEREMPUAN</b></td>
+                </tr>
+                <tr>
+                    <td><b>Alamat : JL BIDO NO.3</b></td>
+                </tr>
+                <tr>
+                    <td><b>RT/RW : 005/005</b></td>
+                </tr>
+                <tr>
+                    <td><b>Kel/Desa : SUKUN</b></td>
+                </tr>
+                <tr>
+                    <td><b>Kecamatan : SUKUN</b></td>
+                </tr>
+                <tr>
+                    <td><b>Agama : ISLAM</b></td>
+                </tr>
+                <tr>
+                    <td><b>Status Perkawinan : BELUM KAWIN</b></td>
+                    
+                </tr>
+                <tr>
+                    <td><b>Pekerjaan : PELAJAR/MAHASISWA</b></td>
+                    <td class="foto-table"><b>KOTA MALANG<br>
+                            28-01-2021
+                    </b></td>                   
+                </tr> 
+                <tr>
+                    <td><b>Kewarganegaraan : WNI</b></td>
+                </tr>
+                <tr>
+                    <td><b>Berlaku Hingga : SEUMUR HIDUP</b></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td rowspan="1" class="foto-table"><img src="img/ttdku.png" height="60px" width="100px"></td>
+                </tr>
+                <p></p>
+            </div>
+            </div>
+        </div>
+                
+        </div>
+
+    </body>
+</html>
