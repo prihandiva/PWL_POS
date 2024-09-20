a372957e5722211d39a5a00d2a447ee6cd8a9ff4
prihandiva <prihandiva1711@gmail.com>
2024-09-20 20:42:07 +0700

    Jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,16 +4,84 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\UserModel;
-
+use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
 
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username','customer-1')->update($data);//update data user
-    //coba akses model UserModel
+    //     $data = [
+    //         'level_id' => 2,
+    //         'username' => 'manager_tiga',
+    //         'nama' => 'Manager 3',
+    //         'password' => Hash::make('12345')
+    //     ];
+
+    //     UserModel::create($data);
     $user = UserModel::all();//ambil semua data dari tabel m_user
-    return view('user',['data'=> $user]);
-}}
+    // return view('user',['data'=> $user]);
+
+    // $user = UserModel::find(1);
+   // $user = UserModel::where('level_id',1)->first();
+    // $user = UserModel::firstWhere('level_id',1);
+    // $user = UserModel::findOr(1,['username','nama'],function(){
+    //     abort(404);
+    // });
+    // $user = UserModel::findOr(20,['username','nama'],function(){
+    //     abort(404);
+    // });
+    // // $user = UserModel::findOrFail(1);
+    // $user=UserModel::where('username','manager9')->firstOrFail();
+    // $user = UserModel::where('level_id',2)->count();
+    // dd($user);
+    // return view('user',['data' => $user]);
+
+    //Menampilkan jumlah pengguna
+    // $userCount = UserModel::where('level_id', 2)->count(); // Hitung jumlah pengguna dengan level_id 2
+
+
+        // $user = UserModel::create(
+        //     [
+        //         'username' => 'manager11',
+        //         'nama'=> 'Manager11',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2,
+        //     ],
+        //     );
+        //     $user->username = 'manager12';
+
+        //     // $user->isDirty(); // true
+        //     // $user->isDirty('username'); // true $user->isDirty('nama'); // false $user->isDirty(['nama', 'username']); // true
+        //     // $user->isclean(); // false
+        //     // $user->isClean ('username'); // false
+        //     // $user->isClean ('nama'); // true
+        //     // $user->isClean (['nama', 'username']); // false
+        
+        //     $user->save();
+        //     $user->wasChanged();
+        //     $user->wasChanged('username');
+        //     $user->wasChanged(['username','level_id']);
+        //     $user->wasChanged('nama');
+        //     dd($user->wasChanged(['nama','username']));
+            
+            
+        //     // $user->isDirty(); // false
+        //     // $user->isClean(); // true.
+
+    return view('user', ['data' => $user]); // Kirim hasil ke view
+
+}
+    public function tambah(){
+        return view('user_tambah');
+    }
+    public function tambah_simpan(Request $request){
+        
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request ->nama,
+            'password' => Hash::make('$request->password'),
+            'level_id' => $request->level_id
+        
+        ]);
+        return redirect('/user');
+    }
+}
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,22 +4,38 @@
         <title>Data user Pengguna</title>
     </head>
     <body>
+        {{-- //Tampilkan Data user --}}
         <h1>Data user Pengguna</h1>
+        <a href="/user/tambah">+ Tambah User</a>
+        <br>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
                 <th>ID</th>
                 <th>Username</th>
                 <th>Nama user</th>
                 <th>ID Leevel user</th>
+                <th>Aksi</th>
             </tr>
             @foreach ($data as $d)
             <tr>
-                <td>{{ $d->user_id}}</td>
+            <td>{{ $d->user_id}}</td>
                 <td>{{ $d->username}}</td>
                 <td>{{ $d->nama}}</td>
                 <td>{{ $d->level_id}}</td>
+                <td><a href="/user/ubah/{{$d->user_id}}">Ubah || </a><a href="/user/hapus/{{$d->user_id}}">Hapus</a></td>
             </tr>
-            @endforeach
-        </table>
+           @endforeach
+        </table> 
+
+        {{-- <table border="1" cellpadding="2" cellspacing="0">
+            <tr>
+                <th>Jumlah Pengguna</th>
+                
+            </tr>
+            <tr>
+                <td>{{ $data}}</td>
+            </tr>
+           
+        </table>--}}
     </body>
 </html>
